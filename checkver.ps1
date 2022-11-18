1cd6bd7dfd62c424af52f8739e60276f3a26f912
x004123 <dimitri.kleyn@fv.nrw.de>
2022-11-18 18:47:36 +0100

    move checkver to local.

--- checkver.ps1
+++
@@ -1,5 +0,0 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-$checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
-$cmd = "'$checkver' -dir '$psscriptroot' $($args | foreach { "$_ " })"
-write-host $cmd
-Invoke-Expression -command "& $cmd"
